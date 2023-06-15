268f85106557dacb62ca074083cb23f961439180
Jovanni Schneider <jovanni.schneider@usach.cl>
2023-06-15 02:00:33 -0400

    [HU1 y HU2]

---
+++ src/main/java/cl/usach/tingeso/valoresservice/ValoresServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.usach.tingeso.valoresservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValoresServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValoresServiceApplication.class, args);
+	}
+
+}
